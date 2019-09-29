ab7fe370f86812530c0e4bc55f936295250831a9
Jhih-Ming Huang <jm@byzantine-lab.io>
2019-09-29 11:02:52 +0800

    add owner addresses and mainnet config (#1)

--- deploy.config.js
+++ deploy.config.js
@@ -1,5 +1,51 @@
 module.exports = {
   configs: [
-    {owner: 'test', address: '0x0D54AF942d6bF13870F5CA65D470954f21D3cBE5'},
+    {owner: 'AuthMe', address: '0xC89F8774Fc15070d9419e4AC125eb59aA848BEEa'},
+    {owner: 'KryptoGo', address: '0xf1EB4a03664D6D837d1F7b22E7775565a05dc3Cd'},
+    {owner: 'BiiLabs', address: '0x243123A345b9F21fbAE0860aF07Eb923DA68457f'},
+    {owner: 'portto', address: '0xaB6E532cDaa543662E5E8FbcEe51d39344822258'},
+    {owner: 'Pelith', address: '0x8bf5A93707CA61bf5B60a1C8e9B5433cfAfd71f9'},
+    {owner: 'SOLA_Bloc', address: '0xc1e77d54005521810699Fca5f8DC78De442b02e5'},
+    {
+      owner: 'Tokamak_Labs',
+      address: '0xD2517E40Cb0D2a35FE6749E1387DeBeCc5e6E7c8',
+    },
+    {owner: 'Mithril', address: '0x7b4c91519dA413b22207c3ac7F90280466F2a8bB'},
+    {owner: 'BuckChaf', address: '0xa9DB8Eba70F26243bB26DBC482f1320F4d442D91'},
+    {
+      owner: 'NCKU_DLTLab',
+      address: '0xD379aeDA5d0BC37b4E068585114AfC0Fe5d980C3',
+    },
+    {owner: 'ABC_Lab', address: '0xB620c29f9284005D594141805c0943E15401691e'},
+    {owner: 'BitoEX', address: '0x1F311A45953f2eE85542EB241823d598A94136fE'},
+    {owner: 'CoolBitX', address: '0x097d953d86717f7d022d75CC63530b7A466C6F85'},
+    {
+      owner: 'YZU_CIS_Lab',
+      address: '0x6Dbc2E3541ac91029C5C98C7727a6E389fE3530a',
+    },
+    {
+      owner: 'Fast_Crypto_Lab',
+      address: '0x7384D8b8C5855c0D0c5903Fed0d6c7064539eB32',
+    },
+    {
+      owner: 'Dapp_Pocket',
+      address: '0x4572237eBe0b124E5bAC0BD67c3363016ca4118d',
+    },
+    {owner: 'NCHU_ECC', address: '0x99fA0DD60744473E14F10663152684E90DDc432f'},
+    {owner: 'BSOS', address: '0x681030d0762b51aA5a15cc4b97713152AdcD870c'},
+    {owner: 'Lootex', address: '0x0F0a9806973E2a98aB473702EF997448764D097A'},
+    {
+      owner: 'FCU_PDPLAB',
+      address: '0x03bCC25c39F1445c594b53f3d7d9B655a0e71662',
+    },
+    {
+      owner: 'BlockTempo',
+      address: '0x197AD3bc0112Ea6143A8dc212257CD8934a551fD',
+    },
+    {owner: 'LiMaGo', address: '0xF1722bD52956F6Ab0334E37eAD82Fe0eB0ecC40C'},
+    {
+      owner: 'Blocktimes',
+      address: '0x82E1413528C6b29d1b889dB7bb420DaC48070B95',
+    },
   ],
 };
